0d2ca2623a8c4b4e33e10a1446933d6d8818ebde
vriti-shivang <63708886+vriti-shivang@users.noreply.github.com>
2020-06-11 09:49:09 +0530

    Add files via upload

---
+++ Python/itertools.permutations().py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from itertools import permutations
+s,n = input().split()
+print(*[''.join(i) for i in permutations(sorted(s),int(n))],sep='\n')
