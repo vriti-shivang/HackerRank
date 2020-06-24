2ab85032e3056cec0a764ca26bee45280484524b
vriti-shivang <63708886+vriti-shivang@users.noreply.github.com>
2020-06-24 21:07:56 +0530

    Add files via upload

---
+++ Python/Set .symmetric_difference() Operation.py
@@ -0,0 +1,4 @@
+_, a = input(), set(input().split())
+_, b = input(), set(input().split())
+print(len(a.symmetric_difference(b)))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
